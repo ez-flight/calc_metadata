5908abe6ef6082fe34f0f1731a22f13a2b1f4dca
Ez <ez.flight@yandex.ru>
2025-02-25 17:06:53 +0300

    Первое приближение

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-# Программа построения Графика зависимости частоты Угла места от времени
 import math
 from datetime import date, datetime, timedelta
 
-# Не забываем импортировать matplotlib.pyplot
+# Библиотека графиков
 import matplotlib.pyplot as plt
 import numpy as np
 import shapefile
+import xlwt
 # Ключевой класс библиотеки pyorbital
 from pyorbital.orbital import Orbital
 
@@ -35,7 +35,7 @@ def get_position(tle_1, tle_2, utc_time):
     return X_s, Y_s, Z_s, Vx_s, Vy_s, Vz_s
 
 
-def create_orbital_track_shapefile_for_day(tle_1, tle_2, pos_t, dt_start, dt_end, delta, track_shape, Fd):
+def create_orbital_track_shapefile_for_day(tle_1, tle_2, dt_start, dt_end, delta, track_shape,pos_gt, Fd):
  
     # Угловая скорость вращения земли
     We = 7.2292115E-5
@@ -44,17 +44,22 @@ def create_orbital_track_shapefile_for_day(tle_1, tle_2, pos_t, dt_start, dt_end
     # Длина волны
     Lam=0.000096
     # Координаты объекта в геодезической СК
-    lat_t, lon_t, alt_t = pos_t
-
+    lat_t, lon_t, alt_t = pos_gt
     # Время начала расчетов
     dt = dt_start
 
-    time_mass = []
-    a_mass = []
-    R_0_mass = []
-    lat_mass = []
-    lon_mass = []
-
+    i_m = []
+    dt_m = []
+    lon_s_m = []
+    lat_s_m = []
+    R_s_m = []
+    R_e_m = []
+    R_0_m = []
+    y_grad_m = []
+    ay_grad_m = []
+    a_m = []
+    Wp_m = []
+    Fd_m = []
 
     # Объявляем счётчики, i для идентификаторов, minutes для времени
     i = 0
@@ -64,24 +69,20 @@ def create_orbital_track_shapefile_for_day(tle_1, tle_2, pos_t, dt_start, dt_end
         X_s, Y_s, Z_s, Vx_s, Vy_s, Vz_s = get_position(tle_1, tle_2, dt)
         Rs = X_s, Y_s, Z_s
         Vs = Vx_s, Vy_s, Vz_s
+
         # Считаем положение спутника в геодезической СК
         lon_s, lat_s, alt_s = get_lat_lon_sgp(tle_1, tle_2, dt)
-        lon_mass.append(lon_s)
-        lat_mass.append(lat_s)
-        if lat_s < 0.3 and lat_s > -0.3:
-            print(dt)
 
         #Персчитываем положение объекта из геодезической в инерциальную СК  на текущее время с расчетом компонентов скорости точки на земле
-        pos_t, v_t = get_xyzv_from_latlon(dt, lon_t, lat_t, alt_t)
-        X_t, Y_t, Z_t = pos_t
+        pos_it, v_t = get_xyzv_from_latlon(dt, lon_t, lat_t, alt_t)
+        X_t, Y_t, Z_t = pos_it
 
         #Расчет ----
         R_s = math.sqrt((X_s**2)+(Y_s**2)+(Z_s**2))
-        R_0 = 750
- #       R_0 = 561.6
-        #R_0 = math.sqrt(((X_s-X_t)**2)+((Y_s-Y_t)**2)+((Z_s-Z_t)**2))
+        R_0 = math.sqrt(((X_s-X_t)**2)+((Y_s-Y_t)**2)+((Z_s-Z_t)**2))
         R_e = math.sqrt((X_t**2)+(Y_t**2)+(Z_t**2))
         V_s = math.sqrt((Vx_s**2)+(Vy_s**2)+(Vz_s**2))
+        print(R_0)
 
         #Расчет двух углов
         #Верхний (Угол Визирования)
@@ -90,44 +91,70 @@ def create_orbital_track_shapefile_for_day(tle_1, tle_2, pos_t, dt_start, dt_end
         #Нижний (Угол места)
         ay = math.acos(((R_0*math.sin(y))/R_e))
         ay_grad = math.degrees(ay)
-# 
-        date_delta = dt - dt_start
-        time_mass.append(date_delta.total_seconds())
 
-        R_0_mass.append(R_0)
+        if  y_grad > 24 and y_grad < 55 and R_0 < R_e:
+            #Расчет угловой скорости вращения земли для подспутниковой точки
+            Wp = 1674 * math.cos(math.radians(lat_s))
+            Wp_m.append(Wp)
+     #       Wp = We * math.cos(lat_t)* Re
+           # Расчет угла a ведется в файле calc_F_L.py резкльтат в градусах
+            a = calc_lamda (Fd, Lam, ay, Rs, Vs, R_0, R_e, R_s, V_s)
 
-        # Расчет угла a ведется в файле calc_F_L.py резкльтат в градусах
-        a = calc_lamda(Fd, Lam, ay, Rs, Vs, R_0, R_s, R_e, V_s)
-        a_mass.append(a)
-
-        # Создаём в шейп-файле новый объект
-        # Определеяем геометрию
-        track_shape.point(lon_s, lat_s)
-        # и атрибуты
-        track_shape.record(i, dt, lon_s, lat_s, R_s, R_e, R_0, y_grad, ay_grad, a, Fd)
-        # Не забываем про счётчики
- #      print(ugol)
+            i_m.append(i)
+            dt_m.append(dt)
+            lon_s_m.append(lon_s)
+            lat_s_m.append(lat_s)
+            R_s_m.append(R_s)
+            R_e_m.append(R_e)
+            R_0_m.append(R_0)
+            y_grad_m.append(y_grad)
+            ay_grad_m.append(ay_grad)
+            a_m.append(a)
+            Fd_m.append(Fd)
+ #           print (f"Частота доплера - {Fd:.0f}, скорость {Wp}")
+ #       print (f"{Fd}")
+  #          print (R_0)
+            # Создаём в шейп-файле новый объект
+            # Определеяем геометрию
+            track_shape.point(lon_s, lat_s)
+            # и атрибуты
+            track_shape.record(i, dt, lon_s, lat_s, R_s, R_e, R_0, y_grad, ay_grad, a, Fd)
+            # Не забываем про счётчики
+ #       print(ugol)
         i += 1
         dt += delta
 
  #   print (i)
-    return track_shape, time_mass, a_mass, R_0_mass, lat_mass, lon_mass
+    return track_shape, i_m, dt_m, lon_s_m, lat_s_m, R_s_m, R_e_m, R_0_m, y_grad_m, ay_grad_m, a_m, Fd_m, Wp_m
    
 
 
 def _test():
 
+    book = xlwt.Workbook(encoding="utf-8")
+
     #25544 37849
     # 56756 Кондор ФКА
     s_name, tle_1, tle_2 = read_tle_base_file(56756)
-    Fd = -5000
-    filename = "Result/Proba" + s_name + ".shp"
-    print (filename)
+    #s_name, tle_1, tle_2 = read_tle_base_internet(37849)
+        
+    # Координаты объекта в геодезической СК
 
     lat_t = 59.95  #55.75583
     lon_t = 30.316667 #37.6173
     alt_t = 12
-    pos_t = [lat_t, lon_t, alt_t]
+    pos_gt_1 = lat_t, lon_t, alt_t
+    pos_gt_2 = (55.75583, 37.6173, 140)
+#    print (pos_gt_1)
+ #   print (pos_gt_2)    
+     
+    Fd = 0
+
+    filename = "result/" + s_name
+#    filename1 = "space1/" + str(a) + "_" + s_name
+
+
+    print (filename)
     # Создаём экземпляр класса Writer для создания шейп-файла, указываем тип геометрии
     track_shape = shapefile.Writer(filename, shapefile.POINT)
 
@@ -148,7 +175,7 @@ def _test():
 
      
     #Задаем начальное время
-    dt_start = datetime(2024, 2, 21, 19, 57, 00)
+    dt_start = datetime(2024, 2, 21, 3, 0, 0)
     #Задаем шаг по времени для прогноза
     delta = timedelta(
         days=0,
@@ -162,50 +189,44 @@ def _test():
 
     #Задаем количество суток для прогноза
     dt_end = dt_start + timedelta(
-        days=0,
-        seconds=5689,
-#        seconds=0,
+        days=16,
+#        seconds=5689,
+        seconds=0,
         microseconds=0,
         milliseconds=0,
         minutes=0,
         hours=0,
         weeks=0
     )
-
-    time_mass = []
-    a_mass = []
-    R_0_mass = []
-    lat_mass = []
-    lon_mass = []
     
-    
-    while  Fd <= 5000:
-        track_shape, time_m, Fd_m, R_0_m, lat_m, lon_m = create_orbital_track_shapefile_for_day(tle_1, tle_2, pos_t, dt_start, dt_end, delta, track_shape, Fd)
-        time_mass.append(time_m)
-        a_mass.append(Fd_m)
-        R_0_mass.append(R_0_m)
-        lon_mass.append(lon_m)
-        lat_mass.append(lat_m)
-        Fd += 5000
+    track_shape, i_m, dt_m, lon_s_m, lat_s_m, R_s_m, R_e_m, R_0_m, y_grad_m, ay_grad_m, a_m, Fd_m_1, Wp_m_1,  = create_orbital_track_shapefile_for_day(tle_1, tle_2, dt_start, dt_end, delta, track_shape, pos_gt_1, Fd)
 
+    sheet1 = book.add_sheet(str(Fd))   
+    for num in range(len(Fd_m_1)):
+
+        row = sheet1.row(num)
+        row.write(0, i_m[num])
+        row.write(1, dt_m[num])
+        row.write(2, lon_s_m[num])
+        row.write(3, lat_s_m[num])
+        row.write(4, R_s_m[num])
+        row.write(5, R_e_m[num])
+        row.write(6, R_0_m[num])
+        row.write(7, y_grad_m[num])
+        row.write(8, ay_grad_m[num])
+        row.write(9, a_m[num])
+        row.write(10, Fd_m_1[num])
+        row.write(11, Wp_m_1[num])
         
-    # Создали объекты окна fig
-    fig, (gr_1, gr_2) = plt.subplots(nrows=2)
-    # Задали расположение графиков в 2 строки
-    gr_1.plot(lat_mass[0], a_mass[0], 'r', linestyle='--', label="Частота $Fd$ = -15 КГц")
-    gr_1.plot(lat_mass[1], a_mass[1], 'g', label="Частота $Fd$ = 0 КГц")
-    gr_1.plot(lat_mass[2], a_mass[2],  'b', linestyle='dotted', label="Частота $Fd$ = +15 КГц")
-    gr_2.plot(lat_mass[0], time_mass[0],  'y')
-    # Подписываем оси, пишем заголовок
-    #   gr_1.set_title('Доплеровское смещение частоты отраженного сигнала в зависимости времени')
-    gr_1.set_ylabel('α, град')
-    gr_2.set_ylabel('Время, сек')
-    gr_2.set_xlabel(' Широта, град')
-    gr_1.legend()
-    # Отображаем сетку
-    gr_1.grid(True)
-    gr_2.grid(True)
-    plt.show()
+    
+
+    book.save(filename + "_Fd" + ".xls")
+  #  plt.title('Доплеровское смещение частоты отраженного сигнала в зависимости от угла скоса и угловой скорости подспутниковой точки')
+  #  plt.xlabel('скорость подспутниковой точки')
+  #  plt.ylabel('Fd,Гц')
+  #  plt.plot( a_m, Wp_m_1, 'ro')
+   # plt.show()
+
 
     # Вне цикла нам осталось записать созданный шейп-файл на диск.
     # Т.к. мы знаем, что координаты положений ИСЗ были получены в WGS84
@@ -222,7 +243,7 @@ def _test():
         # И закрываем его
         prj.close()
         # Функцией save также сохраняем и сам шейп.
-        track_shape.save(filename)
+        track_shape.save(filename + ".shp")
     except:
         # Вдруг нет прав на запись или вроде того...
         print("Unable to save shapefile")
